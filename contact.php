eb2e950b463df192d341089b760822597f5ab4ff
Annemiek Veld <annemiek.veld@student.nhlstenden.com>
2025-10-10 10:56:58 +0200

    Added the fonts

--- contact.php
+++ contact.php
@@ -8,7 +8,25 @@
     </head>
     <body>
         <div class="main">
-            <h1>Contact us</h1>
+            <div class="info-left">
+                <div class="opening-hours">
+                    <ul>
+                        <li>Mon - Closed</li>
+                        <li>Tues - 9:00/17:00</li>
+                        <li>Wed - 9:00/17:00</li>
+                        <li>Thur - 9:00/17:00</li>
+                        <li>Fri - 9:00/17:00</li>
+                        <li>Sat - 9:00/17:00</li>
+                        <li>Sun - 9:00/17:00</li>
+                    </ul>
+                </div>
+
+                <p><a href="mailto:emmen@oui-cafe.fr">📩 emmen@oui-cafe.fr</a></p>
+                <p><a href="tel:+31631782094">📞 +31 6 31782094</a></p>
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4826.873940502982!2d6.91088217693991!3d52.77842821706407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7e61f3ec72443%3A0xbe9d297b3e4fbcc7!2sNHL%20Stenden%20Hogeschool!5e0!3m2!1snl!2snl!4v1760084667689!5m2!1snl!2snl" 
+                    width="250" height="165" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
         </div>
     </body>
 </html>
