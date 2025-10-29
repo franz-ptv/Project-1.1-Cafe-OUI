8d312cd1d2cb1ac88a731252e3403f891b2a124c
JP <jp.moro.machado@student.nhlstenden.com>
2025-10-29 03:54:57 +0100

    transfered all code from draf to actual impression php, finished impression/exhibition page

--- impression.php
+++ impression.php
@@ -3,9 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="impression_style.css">
     <title>Impression</title>
 </head>
-<body>
-    <p>yoyoyo</p>
+<body class="impression">
+    <section class="exhibition"
+     style="background: linear-gradient(rgba(149,38,31,0.6), rgba(149,38,31,0.6)),
+         url('images/impression/exhibition.png') center/cover no-repeat;">
+
+        <div class="exhibition-header">
+            <h3>First try checking our</h3>
+            <h1>EXHIBITION</h1>
+        </div>
+
+        <div class="exhibition-gallery">
+            <div class="artwork">
+                <img src="images/impression/jeune_fille_au_ruban_rouge.png" alt="Jeune Fille au Ruban Rouge">
+                <h4>Éloise Marchand (1768-1729)</h4>
+                <p>1796</p>
+            </div>
+            <div class="artwork">
+                <img src="images/impression/la_serenite_dautomne.png" alt="La Sérénité d'Automne">
+                <h4>Henri Delacroix (1779-1847)</h4>
+                <p>1812</p>
+            </div>
+            <div class="artwork">
+                <img src="images/impression/la_demenite_dautomne.png" alt="La Démenité d'Automne">
+                <h4>Marguerite Lefèvre (1725-1791)</h4>
+                <p>1758</p>
+            </div>
+            <div class="artwork">
+                <img src="images/impression/la_legende.png" alt="La Légende">
+                <h4>Alexandre Vautrin (1742-1805)</h4>
+                <p>1784</p>
+            </div>
+        </div>
+    <div class="border1"></div>
+    <div class="border2"></div>
+    <div class="border3"></div>
+    </section>
+
+    <section class="impression"
+    style= "background: linear-gradient(rgba(149,38,31,0.6), rgba(149,38,31,0.6)),
+    url('images/impression/impression.png') center/cover no-repeat;">
+        <div class="impression-header">
+            <div class="border4"></div>
+            <h1>Always doing our best for a good impression!</h1>
+        </div>
+        <div class="gallery-grid">
+            <img class="img-croissant" src="images/impression/small_croissant.png" alt="croissant">
+            <img class="img-tea-powder" src="images/impression/small_tea_powder.png" alt="tea-powder">
+            <img class="img-coffee" src="images/impression/small_coffee.png" alt="coffee">
+            <img class="img-tea" src="images/impression/small_tea.png" alt="tea">
+            
+            <div class="impression-player">
+                <iframe style="border-radius:12px;" 
+                    src="https://open.spotify.com/embed/playlist/3NOU6rfchhZ6B0u7BWlbuq?utm_source=generator" 
+                    width="100%" height="152" frameborder="0" 
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                    loading="lazy"></iframe>
+            </div>
+            
+            <img class="img-cappuccino" src="images/impression/small_cappuccino.png" alt="cappuccino">
+            <img class="img-merengue" src="images/impression/small_merengue.png" alt="merengue">
+        </div>
+    </section>
 </body>
 </html>
