42c9acac1d57121291535a4fb166af704dae9a5d
franz <franz.petev@student.nhlstenden.com>
2025-10-30 10:42:12 +0100

    index in correct place

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalabe=no ">
+    <title>TryPHP</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link href='https://fonts.googleapis.com/css?family=Caveat' rel='stylesheet'>
+</head>
+
+<body>
+
+    <nav class = "navbar">
+    <ul class="navbarul">
+
+      <div class="navlogo">
+       <a class = "logo" href="index.php">OUI</a>
+      </div>
+
+      <div>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="menu.php">Menu</a></li>
+        <li><a href="aboutus.php">About Us</a></li>
+        <li><a href="impression.php">Impression</a></li>
+        <li><a href="contact.php">Contact us</a></li>
+      </div>
+
+      <div>
+        <button>Language</button>
+        <button>Dark/Light</button>
+      </div>
+
+    </ul>
+  </nav>
+
+  <div class = "firstimg">
+      <div class = "FirstText">
+      Café OUI
+      </div>
+
+      <div class = "SecondText">
+        Café Oui - Say yes to coffee, say yes to life.
+      </div>
+  </div>
+
+  <div class = "secondimg">
+    <div class="line_blue"></div>
+    <div class="line_blue"></div>
+      <div class = "mid_screen">
+        <div><p class="cest">C'est delicieux!</p></div>
+          <a href="menu.php"><button class = "menu_button" >Visit our menu</button></a>
+        <div><p class="delicious">Certified delicious!</p></div>
+      </div>
+  <img src="croissant.png" class="croissant_pic" alt="">
+  <img src="treat.png" class="treat_pic" alt="">
+  <img src="i_croissant.png" class="croissant_png" alt="">
+  <img src="i_treat.png" class="treat_png" alt="">
+  <img src="i_tea.png" class="tea_png" alt="">
+  <img src="i_coffee.png" class="coffee_png" alt="">
+  </div>
+
+  <div class = "textbox">
+    <div class="line_beige"></div>
+    <div class="line_beige"></div>
+    <a href="exhibition.php"><p class="link_exh">Click here to check our exhibition!</p></a>
+    <p class="pared">A little bit of our French culture in your favourite cafe!</p>
+  </div>
+
+  <footer>
+    <div class = "footer_container">
+
+      <div class = "box1">
+        <div class = "footer_links_row">
+          <a href="index.php">Home</a>
+          <a href="index.php">About us</a>
+          <a href="index.php">Contact us</a>
+        </div>
+        <div class = "footer_links_row">
+          <a href="index.php">Menu</a>
+          <a href="index.php">Impression</a>
+        </div>
+      </div>
+
+      <div class = "box2">
+        <a href="index.php" class = "logo_footer">OUI</a>
+        <div class = "social_icons">
+          <a href="https://facebook.com" target="_blank">
+            <img src="facebook-svgrepo-com.svg" alt="Facebook icon">
+          </a>
+          <a href="https://twitter.com" target="_blank">
+            <img src="twitter-color-svgrepo-com.svg" alt="Twitter icon">
+          </a>
+          <a href="https://instagram.com" target="_blank">
+            <img src="instagram-1-svgrepo-com.svg" alt="Instagram icon">
+          </a>
+        </div>
+      </div>
+
+      <div class = "box3">
+        <h3>Adress: Van Schaikweg 94, 7811KL Emmen</h3>
+        <h4>Working hours:</h4>
+        <ul>
+          <li>Mon - Close</li>
+          <li>Tues - 09:00 - 17:00</li>
+          <li>Wed - 09:00 - 17:00</li>
+          <li>Thurs - 09:00 - 17:00</li>
+          <li>Fri - 09:00 - 17:00</li>
+          <li>Sat - 09:00 - 17:00</li>
+          <li>Sun - 09:00 - 17:00</li>
+        </ul>
+      </div>
+
+    </div>
+  </footer>
+  
+</body>
+</html>
