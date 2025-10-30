777f46567fb0c6cd06beeb811c0627b213d65851
Annemiek Veld <annemiek.veld@student.nhlstenden.com>
2025-10-30 10:06:03 +0100

    indentations

--- 404.php
+++ 404.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body class="main-error">
-    <div class="error">
-        <h1>404!</h1>
-        <h2>Something went wrong, we couldn't find what you were looking for.</h2>
-        <h2>We are sorry for that, please take a croissant while you go back to the homepage.</h2>
-        <a href="index.php">Go back home!</a>
-    </div>
-
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>404</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body class="main-error">
+        <div class="error">
+            <h1>404!</h1>
+            <h2>Something went wrong, we couldn't find what you were looking for.</h2>
+            <h2>We are sorry for that, please take a croissant while you go back to the homepage.</h2>
+            <a href="index.php">Go back home!</a>
+        </div>
+    </body>
 </html>
