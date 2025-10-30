c7cbe0c0d211102559538f84de63adfc44cc7a30
franz <franz.petev@student.nhlstenden.com>
2025-10-30 23:54:32 +0100

    Implementing translated pages of index into dutch and french

---
+++ indexFR.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalabe=no ">
+    <title>Café OUI</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link href='https://fonts.googleapis.com/css?family=Caveat' rel='stylesheet'>
+    <link rel="icon" type="image/x-icon" href="images/navigation-bar/fav.png">
+</head>
+
+<body>
+
+    <nav class = "navbar">
+    <ul class="navbarul">
+
+      <div class="navlogo">
+       <a class = "logo" href="index.php">OUI</a>
+      </div>
+
+      <div>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="menu.php">Menu</a></li>
+        <li><a href="about_us.php">À propos</a></li>
+        <li><a href="impression.php">Impression</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </div>
+
+      <div>
+        <button>Language</button>
+        <button>Dark/Light</button>
+      </div>
+
+    </ul>
+  </nav>
+
+  <div class = "firstimg">
+      <div class = "FirstText">
+      Café OUI
+      </div>
+
+      <div class = "SecondText">
+        Café Oui - Dites oui au café, dites oui à la vie.
+      </div>
+  </div>
+
+  <div class = "secondimg">
+    <div class="line_blue"></div>
+    <div class="line_blue"></div>
+      <div class = "mid_screen">
+        <div><p class="cest">C'est delicieux!</p></div>
+          <a href="menu.php"><button class = "menu_button" >Découvrez notre menu</button></a>
+        <div><p class="delicious">Incroyablement bon!</p></div>
+      </div>
+  <img src="images/home-page/croissant.png" class="croissant_pic" alt="">
+  <img src="images/home-page/treat.png" class="treat_pic" alt="">
+  <img src="images/home-page/i_croissant.png" class="croissant_png" alt="">
+  <img src="images/home-page/i_treat.png" class="treat_png" alt="">
+  <img src="images/home-page/i_tea.png" class="tea_png" alt="">
+  <img src="images/home-page/i_coffee.png" class="coffee_png" alt="">
+  </div>
+
+  <div class = "textbox">
+    <div class="line_beige"></div>
+    <div class="line_beige"></div>
+    <a href="impression.php"><p class="link_exh">Cliquez ici pour voir notre exposition!</p></a>
+    <p class="pared">Un peu de culture française dans votre café préféré!</p>
+  </div>
+
+  <footer>
+    <div class = "footer_container">
+
+      <div class = "box1">
+        <div class = "footer_links_row">
+          <a href="index.php">Home</a>
+          <a href="about_us.php">À propos</a>
+          <a href="contact.php">Contact</a>
+        </div>
+        <div class = "footer_links_row">
+          <a href="menu.php">Menu</a>
+          <a href="impression.php">Impression</a>
+        </div>
+      </div>
+
+      <div class = "box2">
+        <a href="index.php" class = "logo_footer">OUI</a>
+        <div class = "social_icons">
+          <a href="https://facebook.com" target="_blank">
+            <img src="images/home-page/facebook-svgrepo-com.svg" alt="Facebook icon">
+          </a>
+          <a href="https://twitter.com" target="_blank">
+            <img src="images/home-page/twitter-color-svgrepo-com.svg" alt="Twitter icon">
+          </a>
+          <a href="https://instagram.com" target="_blank">
+            <img src="images/home-page/instagram-1-svgrepo-com.svg" alt="Instagram icon">
+          </a>
+        </div>
+      </div>
+
+      <div class = "box3">
+        <h3>Adresse: Van Schaikweg 94, 7811KL Emmen</h3>
+        <h4>Heures d'ouverture:</h4>
+        <ul>
+          <li>Lun - Close</li>
+          <li>Mar - 09:00 - 17:00</li>
+          <li>Mer - 09:00 - 17:00</li>
+          <li>Jeu - 09:00 - 17:00</li>
+          <li>Ven - 09:00 - 17:00</li>
+          <li>Sam - 09:00 - 17:00</li>
+          <li>Dim - 09:00 - 17:00</li>
+        </ul>
+      </div>
+
+    </div>
+  </footer>
+  
+</body>
+</html>
