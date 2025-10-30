cfe8f2c95ce44f3187018b21fe60fa76ca4d77fb
Annemiek Veld <annemiek.veld@student.nhlstenden.com>
2025-10-30 14:14:23 +0100

    added navbar and footer

--- contactNL.php
+++ contactNL.php
@@ -34,9 +34,34 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Contact</title>
-        <link rel="stylesheet" href="style.css" type="text/css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="stylesheet.css">
     </head>
-    <body>
+    <body class="contact-body">
+        
+        <nav class = "navbar">
+            <ul class="navbarul">
+
+            <div class="navlogo">
+            <a class = "logo" href="index.php">OUI</a>
+            </div>
+
+            <div>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="about_us.php">About Us</a></li>
+                <li><a href="impression.php">Impression</a></li>
+                <li><a href="contact.php">Contact us</a></li>
+            </div>
+
+            <div>
+                <button>Language</button>
+                <button>Dark/Light</button>
+            </div>
+
+            </ul>
+        </nav>
+
         <div class="main">
             <div class="info-left">
                 <div class="opening-hours">
@@ -50,9 +75,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
                         <li>Zon - 9:00/17:00</li>
                     </ul>
                 </div>
-
-                <p><a href="mailto:emmen@oui-cafe.fr">📩 emmen@oui-cafe.fr</a></p>
-                <p><a href="tel:+31631782094">📞 +31 6 31782094</a></p>
+                <div class="link">
+                    <p><a href="mailto:emmen@oui-cafe.fr">📩 emmen@oui-cafe.fr</a></p>
+                    <p><a href="tel:+31631782094">📞 +31 6 31782094</a></p>
+                </div>
+                
 
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4826.873940502982!2d6.91088217693991!3d52.77842821706407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7e61f3ec72443%3A0xbe9d297b3e4fbcc7!2sNHL%20Stenden%20Hogeschool!5e0!3m2!1snl!2snl!4v1760084667689!5m2!1snl!2snl"
                     width="250" height="165" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
@@ -82,5 +109,52 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
                 ?>
             </div>
         </div>
+
+        <footer>
+            <div class = "footer_container">
+
+            <div class = "box1">
+                <div class = "footer_links_row">
+                <a href="index.php">Home</a>
+                <a href="about_us.php">About us</a>
+                <a href="contact.php">Contact us</a>
+                </div>
+                <div class = "footer_links_row">
+                <a href="menu.php">Menu</a>
+                <a href="impression.php">Impression</a>
+                </div>
+            </div>
+
+            <div class = "box2">
+                <a href="index.php" class = "logo_footer">OUI</a>
+                <div class = "social_icons">
+                <a href="https://facebook.com" target="_blank">
+                    <img src="images/home-page/facebook-svgrepo-com.svg" alt="Facebook icon">
+                </a>
+                <a href="https://twitter.com" target="_blank">
+                    <img src="images/home-page/twitter-color-svgrepo-com.svg" alt="Twitter icon">
+                </a>
+                <a href="https://instagram.com" target="_blank">
+                    <img src="images/home-page/instagram-1-svgrepo-com.svg" alt="Instagram icon">
+                </a>
+                </div>
+            </div>
+
+            <div class = "box3">
+                <h3>Adress: Van Schaikweg 94, 7811KL Emmen</h3>
+                <h4>Working hours:</h4>
+                <ul>
+                <li>Mon - Close</li>
+                <li>Tues - 09:00 - 17:00</li>
+                <li>Wed - 09:00 - 17:00</li>
+                <li>Thurs - 09:00 - 17:00</li>
+                <li>Fri - 09:00 - 17:00</li>
+                <li>Sat - 09:00 - 17:00</li>
+                <li>Sun - 09:00 - 17:00</li>
+                </ul>
+            </div>
+
+            </div>
+        </footer>
     </body>
 </html>
