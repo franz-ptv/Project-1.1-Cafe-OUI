f6ce00ba559654216eb225a42410c7db90087b12
franz <franz.petev@student.nhlstenden.com>
2025-10-31 20:41:08 +0100

    Moved style.css in css folder. Connected to contact us page.

--- contact.php
+++ contact.php
@@ -34,7 +34,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Contact</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="stylesheet.css">
         <link rel="icon" type="image/x-icon" href="images/navigation-bar/fav.png">
     </head>
