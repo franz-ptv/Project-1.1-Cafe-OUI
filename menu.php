605dede9842688fde17bfcc7118ce28a6e97fed0
Tanyi mary <tabi.tanyi@student.nhlstenden.com>
2025-11-01 11:02:05 +0100

    make changes

--- menu.php
+++ menu.php
@@ -7,8 +7,34 @@
     <title>Menu Page</title>
 </head>
 <body>
+  <header>
+    <nav class = "navbar">
+    <ul class="navbarul">
 
-     <!-- Green Tea section -->
+      <div class="navlogo">
+       <a class = "logo" href="index.php">OUI</a>
+      </div>
+
+      <div>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="menu.php">Menu</a></li>
+        <li><a href="about_us.php">About Us</a></li>
+        <li><a href="impression.php">Impression</a></li>
+        <li><a href="contact.php">Contact us</a></li>
+      </div>
+
+      <div>
+        <button>Language</button>
+        <button>Dark/Light</button>
+      </div>
+
+    </ul>
+  </nav>
+  </header>
+
+
+
+  <!-- Green Tea section -->
 <div class="menu-container">
   <div class="top-left">
     
@@ -123,6 +149,56 @@
     <button>download menu</button>
   </div>
 </div>
+
+ <footer>
+    <div class = "footer_container">
+
+      <div class = "box1">
+        <div class = "footer_links_row">
+          <a href="index.php">Home</a>
+          <a href="about_us.php">About us</a>
+          <a href="contact.php">Contact us</a>
+        </div>
+        <div class = "footer_links_row">
+          <a href="menu.php">Menu</a>
+          <a href="impression.php">Impression</a>
+        </div>
+      </div>
+
+      <div class = "box2">
+        <a href="index.php" class = "logo_footer">OUI</a>
+        <div class = "social_icons">
+          <a href="https://facebook.com" target="_blank">
+            <img src="images/home-page/facebook-svgrepo-com.svg" alt="Facebook icon">
+          </a>
+          <a href="https://twitter.com" target="_blank">
+            <img src="images/home-page/twitter-color-svgrepo-com.svg" alt="Twitter icon">
+          </a>
+          <a href="https://instagram.com" target="_blank">
+            <img src="images/home-page/instagram-1-svgrepo-com.svg" alt="Instagram icon">
+          </a>
+        </div>
+      </div>
+
+      <div class = "box3">
+        <h3>Adress: Van Schaikweg 94, 7811KL Emmen</h3>
+        <h4>Working hours:</h4>
+        <ul>
+          <li>Mon - Close</li>
+          <li>Tues - 09:00 - 17:00</li>
+          <li>Wed - 09:00 - 17:00</li>
+          <li>Thurs - 09:00 - 17:00</li>
+          <li>Fri - 09:00 - 17:00</li>
+          <li>Sat - 09:00 - 17:00</li>
+          <li>Sun - 09:00 - 17:00</li>
+        </ul>
+      </div>
+
+    </div>
+  </footer>
+  
+
+
 
 </body>
 </html>
